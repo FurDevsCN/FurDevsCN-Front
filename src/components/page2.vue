50365b2bdc7b3133419b88867197f0464592d06e
yinlinghunjiang <1581647952@qq.com>
2022-11-04 22:06:05 +0800

    :lipstick: Some small changes :)

--- src/components/page2.vue
+++ src/components/page2.vue
@@ -8,14 +8,25 @@
                 <div>
                     <div class="card-member-list"></div>
                     <div class="card-member-desc">
-                        <p>排名按照进入组织顺序</p>
-                        <p>统计信息卡片来自于 <a href="https://github.com/anuraghazra/github-readme-stats"
-                                target="_blank">anuraghazra/github-readme-stats</a> 的由玖叁魔改后的 <a
+                        <strong>
+                            <p>排名按照进入组织顺序</p>
+                            <p>统计信息卡片来自于由玖叁魔改后的 <a
                                 href="https://github.com/colour93/github-readme-stats"
                                 target="_blank">colour93/github-readme-stats</a></p>
+                        </strong>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
+<style scoped>
+/* 第二页样式 */
+
+.p-2 {
+  align-items: flex-end;
+  justify-content: center;
+  height: 100%;
+  background-color: transparent;
+
+}</style>
